0ee4bc603a62c10936e07271b1a098f4b98a6ab7
Dylan Ferris <myaka27too@gmail.com>
2020-10-04 21:23:43 -0400

    Fixed GCC compile error (missing predeclares)

--- src/MIPSsim.cpp
+++ src/MIPSsim.cpp
@@ -378,8 +378,6 @@ namespace SPIMDF {
         };
     } // namespace ISA
 
-
-    // Instruction.cpp
     namespace detail {
         template<auto FA, auto FB>
         constexpr std::enable_if_t<FA == FB || FA != FB, bool> SameFunctionPointer() {
@@ -392,12 +390,77 @@ namespace SPIMDF {
         }
     }
 
+    // Microcode.hpp
+    class Instruction;
+
+    #define DEF_EX(X) void X(CPU& cpu, const Instruction& instr);
+    #define DEF_PR(X) std::string X(const Instruction& instr);
+
     class CPU;
 
+    // Predeclare executors and printers
+    namespace Executors {
+        // Category 1
+        DEF_EX(J);
+        DEF_EX(JR);
+        DEF_EX(BEQ);
+        DEF_EX(BLTZ);
+        DEF_EX(BGTZ);
+        DEF_EX(SW); 
+        DEF_EX(LW); 
+        DEF_EX(SLL);
+        DEF_EX(SRL);
+        DEF_EX(SRA);
+        DEF_EX(NOP);
+        DEF_EX(BRK);
+        // Category 2
+        DEF_EX(ADD); 
+        DEF_EX(SUB); 
+        DEF_EX(MUL); 
+        DEF_EX(AND); 
+        DEF_EX(OR);  
+        DEF_EX(XOR); 
+        DEF_EX(NOR); 
+        DEF_EX(SLT); 
+        DEF_EX(ADDI);
+        DEF_EX(ANDI);
+        DEF_EX(ORI); 
+        DEF_EX(XORI);
+    }
 
-    namespace Executors { }
-    namespace Printers { }
+    namespace Printers {
+        // Category 1
+        DEF_PR(J);
+        DEF_PR(JR);
+        DEF_PR(BEQ);
+        DEF_PR(BLTZ);
+        DEF_PR(BGTZ);
+        DEF_PR(SW); 
+        DEF_PR(LW); 
+        DEF_PR(SLL);
+        DEF_PR(SRL);
+        DEF_PR(SRA);
+        DEF_PR(NOP);
+        DEF_PR(BRK);
+        // Category 2
+        DEF_PR(ADD); 
+        DEF_PR(SUB); 
+        DEF_PR(MUL); 
+        DEF_PR(AND); 
+        DEF_PR(OR);  
+        DEF_PR(XOR); 
+        DEF_PR(NOR); 
+        DEF_PR(SLT); 
+        DEF_PR(ADDI);
+        DEF_PR(ANDI);
+        DEF_PR(ORI); 
+        DEF_PR(XORI);
+    }
+
+    #undef DEF_EX
+    #undef DEF_PR
     
+    // Instruction.cpp
     class Instruction {
         public:
         using Executor_g = void(CPU& cpu, const Instruction&);
@@ -954,7 +1017,7 @@ int main(int argc, const char* argv[]) {
     while (true) {
         output << "--------------------\n";
         
-        sprintf(buffer, "Cycle %llu:\t%u\t%s\n\n", cpu.GetCycle(), cpu.GetPC(), cpu.CurInstr().ToString().c_str());
+        sprintf(buffer, "Cycle %lu:\t%u\t%s\n\n", cpu.GetCycle(), cpu.GetPC(), cpu.CurInstr().ToString().c_str());
         output << buffer;
 
         hitBreak = cpu.CurInstr().opcode == ISA::Opcode::BRK;
